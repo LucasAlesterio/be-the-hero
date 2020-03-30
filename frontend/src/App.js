5243585126ed2378a0489f93697cb8e85bba6c42
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2020-03-30 19:22:32 -0300

    front-end(web)

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './routes';
+import './global.css'
+//23:00
+function App() {
+ //Componetes apenas com letra maiuscula
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
